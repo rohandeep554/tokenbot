f639b9194164a666429d89fa7c0326bf3f4b5670
kayyum khan <152464768+onekayyum@users.noreply.github.com>
2024-02-17 17:46:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-#(Â©)CodeXBotz
+
 import asyncio
 import base64
 import logging
@@ -141,13 +141,14 @@ async def start_command(client: Client, message: Message):
             verify_status = await get_verify_status(id)
             if IS_VERIFY and not verify_status['is_verified']:
                 short_url = f"api.shareus.io"
-                full_tut_url = f"https://t.me/leakhubd/1264"
+                full_tut_url = f"
+https://t.me/iamcrieng/3"
                 token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
                 await update_verify_status(id, verify_token=token, link="")
                 link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
                 btn = [
                     [InlineKeyboardButton("Click here", url=link)],
-                    [InlineKeyboardButton('ð³ Tutorial ð³', url=full_tut_url)]
+                    [InlineKeyboardButton(' Tutorial ', url=full_tut_url)]
                 ]
                 await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 Hour after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=True, quote=True)
 
